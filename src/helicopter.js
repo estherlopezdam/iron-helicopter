51a8b0aaa8be99f1396863d49d54552dd5838fba
Julio G <julio@local>
2023-06-24 13:24:34 +0200

    minor

--- src/helicopter.js
+++ src/helicopter.js
@@ -1,36 +1,35 @@
 class Helicopter {
   constructor(ctx) {
-    this.ctx = ctx
-    this.tick = 0
+    this.ctx = ctx;
+    this.tick = 0;
 
-    this.x = 100
-    this.y = 0
+    this.x = 100;
+    this.y = 0;
 
-    this.w = 100
-    this.h = 40
+    this.w = 100;
+    this.h = 40;
 
-    this.x = 0
-    this.y = 0
-    this.vx = 0
-    this.vy = 0
-    this.ay = 0
-    this.ax = 0
-    this.g = 0.1
+    this.x = 0;
+    this.y = 0;
+    this.vx = 0;
+    this.vy = 0;
+    this.ay = 0;
+    this.ax = 0;
+    this.g = 0.1;
 
-    this.img = new Image()
-    this.img.src = "https://2.bp.blogspot.com/-P6ZbLE-rnFM/WPTQh65UtMI/AAAAAAAABF8/9iYl-cIUEtIhj2JDTixyqZNeBn183AdmQCLcB/s1600/helicopter-spritesheet.png"
-    this.img.frames = 4
-    this.img.frameIndex = 0
+    this.img = new Image();
+    this.img.src =
+      "https://2.bp.blogspot.com/-P6ZbLE-rnFM/WPTQh65UtMI/AAAAAAAABF8/9iYl-cIUEtIhj2JDTixyqZNeBn183AdmQCLcB/s1600/helicopter-spritesheet.png";
+    this.img.frames = 4;
+    this.img.frameIndex = 0;
 
-    this.weapon = new Weapon(this)
-
-    this._setListeners()
+    this.weapon = new Weapon(this);
   }
 
   draw() {
     // TODO: draw helicopter image
 
-    this.weapon.draw()
+    this.weapon.draw();
   }
 
   isFloor() {
